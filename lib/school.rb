a1ea09d0b1d0766c22a7d1967dea9049bd63baab
Drew Thompson <drewallynthompson@gmail.com>
2020-09-03 00:05:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,14 @@
 class School 
   
-
+attr_accessor :school, :roster
 
   def initialize(school)
     @school = school
+    @roster = {}
   end
   
-  def roster()
+  def add_student(student, grade)
+    @student = student
+    roster[:student] = 
   
 end
