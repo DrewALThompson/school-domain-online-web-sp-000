c141f409213efa87161e6bae65692dc4af390cc7
Drew Thompson <drewallynthompson@gmail.com>
2020-09-03 00:20:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,7 +18,9 @@ attr_accessor :school, :roster
   
   def sort
     sorted_hash = []
-    @roster.each {|grade, student| sorted_hash[grade] = student.sort }
+    @roster.each do |grade, student| 
+      sorted_hash[grade] = student.sort 
+    end
     sorted_hash
   end
   
